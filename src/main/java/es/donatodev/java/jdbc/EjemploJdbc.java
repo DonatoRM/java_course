2f0f0feb1b036473fd45be5532037193bd146eef
DonatoRM <donatoramos@hotmail.com>
2025-08-19 14:08:31 +0200

    320 - cerrando conexiones, recursos y manejo de errores

--- src/main/java/es/donatodev/java/jdbc/EjemploJdbc.java
+++ src/main/java/es/donatodev/java/jdbc/EjemploJdbc.java
@@ -11,16 +11,19 @@ public class EjemploJdbc {
         String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=Europe/Madrid";
         String username="root";
         String password="donato";
-        try {
-            Connection conn=DriverManager.getConnection(url, username, password);
+        try(Connection conn=DriverManager.getConnection(url, username, password);
             Statement stmt = conn.createStatement();
-            ResultSet resultado= stmt.executeQuery("SELECT * FROM productos");
+            ResultSet resultado= stmt.executeQuery("SELECT * FROM productos")) {
+            
             while(resultado.next()) {
-                System.out.println(resultado.getString("nombre"));
+                System.out.print(resultado.getInt("id"));
+                System.out.print(" | ");
+                System.out.print(resultado.getString("nombre"));
+                System.out.print(" | ");
+                System.out.print(resultado.getInt("precio"));
+                System.out.print(" | ");
+                System.out.println(resultado.getDate("fecha_registro"));
             }
-            resultado.close();
-            stmt.close();
-            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
